6688af7897c9f0c04fd7748c5df4ed52f9cfe151
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2023-01-12 20:11:06 +0200

    Read BIRTHDATE environment variable

--- days.cpp
+++ days.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <string>   // for std::string class
+#include <cstdlib>  // for std::getenv
 
 int main() {
-    using namespace std;
+    // TODO: Get the current date
 
-    cout << "Hello, world!" << endl;
+    // Get the BIRTHDATE environment variable, so that we can
+    // congratulate the user if the current date matches that.
+    const char *birthdateEnv = std::getenv("BIRTHDATE");
+    if (nullptr != birthdateEnv) {  // the BIRTHDATE environment variable exists
+        std::string birthdateString = birthdateEnv;  // assignment operator makes a string out of char *
+        std::cout << "BIRTHDATE = " << birthdateString << std::endl;  // TODO: remove this
+
+        // TODO: Parse the YYYY-MM-DD string, or bail out if the format is wrong
+
+        // TODO: If the birthdate matches the current date, congratulate the user
+    }
+    else {
+        std::cout << "No BIRTHDATE environment variable found" << std::endl;  // TODO: remove this
+    }
 
     return 0;
 }
