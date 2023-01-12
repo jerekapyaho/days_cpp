87f15d9ed4bbf7acd8246569bc1c04a4139571a4
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2023-01-12 22:17:47 +0200

    Create ~/.days directory if necessary

--- days.cpp
+++ days.cpp
@@ -1,8 +1,91 @@
 #include <iostream>
 #include <string>   // for std::string class
 #include <cstdlib>  // for std::getenv
+#include <filesystem>  // for std::filesystem (since C++17)
+
+#if defined(__linux__) || defined(__APPLE__) || defined(__unix__)
+#include <pwd.h>  // for getpwuid()
+#include <unistd.h> // for getuid()
+#endif
+
+// Returns the user's home directory, or empty if it can't be determined.
+std::string getHome() {
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+    // TODO: What about the %APPDATA% environment variable?
+
+    // For Windows, try the USERPROFILE environment variable
+    const char *userProfileEnv = std::getenv("USERPROFILE");
+    if (nullptr != userProfileEnv) {
+        std::string userProfileString = userProfileEnv;
+        return
+        userProfileString.append("\\AppData");
+        userProfileString.append("days");
+    }
+    else {
+        const char *homeDriveEnv = std::getenv("HOMEDRIVE");
+        const char *homePathEnv = std::getenv("HOMEPATH");
+
+        // Concatenate HOMEDRIVE and HOMEPATH, with a backslash between them
+        std::string homeDriveString = homeDriveEnv;
+        homeDriveString.append("\\");
+        homeDriveString.append(homePathEnv);
+
+        // Concatenate the `.days` part
+        homeDriveString.append("\\");
+        homeDriveString.append(".days");
+    }
+
+#else if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+    // Linux and other UNIXes most likely have HOME, macOS might have it
+    const char *homeEnv = std::getenv("HOME");
+    if (nullptr != homeEnv) {  // the HOME environment variable exists
+        return std::string(homeEnv);
+    }
+    else { // try to get the password directory
+        struct passwd *pw = getpwuid(getuid());
+        if (nullptr != pw) {
+            return std::string(pw->pw_dir);
+        }
+        else {  // give up and return the empty string
+            return std::string("");
+        }
+    }
+#endif
+}
+
+// Creates the `~/.days` directory if it doesn't already exist.
+bool createDaysDirIfNecessary() {
+    // Get the user's home directory, if possible
+    auto daysPathString = getHome();
+    if (daysPathString.empty()) {  // bail out
+        std::cerr << "Unable to determine home directory" << std::endl;
+        return -1;
+    }
+    else {
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+        daysPathString.append("\\AppData\\");
+#else
+        daysPathString.append("/.config/days");
+#endif
+    }
+
+    std::cout << "daysPathString = " << daysPathString << std::endl;
+
+    const std::filesystem::path daysPath{daysPathString};
+    if (!std::filesystem::exists(daysPath)) {  // does not yet exist, need to create
+        std::filesystem::create_directory(daysPath);
+        return true;  // true means the directory was created
+    }
+
+    std::cout << "daysPath exists, no need to create" << std::endl;
+
+    return false;  // nothing needed to be done, return false
+}
 
 int main() {
+    // If the `~/.days` directory does not exist, create it
+    createDaysDirIfNecessary();
+
     // TODO: Get the current date
 
     // Get the BIRTHDATE environment variable, so that we can
