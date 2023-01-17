d419a0fa78a767b199f802a04ee5d9df371e8c0a
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2023-01-17 13:31:52 +0200

    Check birthdate against current date, congratulate user

--- days.cpp
+++ days.cpp
@@ -47,15 +47,50 @@ int main() {
                 << " day = " << segmentList.at(2)
                 << std::endl;
 
-            // TODO: If the birthdate matches the current date, congratulate the user
+            // If the birthdate matches the current date, congratulate the user
+            unsigned int birthMonth{0};
+            unsigned int birthDay{0};
+            try {
+                birthMonth = std::stoi(segmentList.at(1));
+                birthDay = std::stoi(segmentList.at(2));
+
+                // If conversion of month or day from string to int failed
+                // with an exception, we should still have zeros in both.
+                // The current date can't have zero month or day, so the
+                // upcoming check produces the correct result.
+
+                // TODO: Remove this after verifying
+                std::cout << "parsed: month = " << birthMonth << " day = " << birthDay << std::endl;
+
+                // Check if month and day of birthdate match those of the current date.
+                if (birthMonth == static_cast<unsigned>(currentDate.month())
+                    && birthDay == static_cast<unsigned>(currentDate.day())) {
+                    std::cout << "Happy birthday";
+
+                    // Get the USER environment variable.
+                    // If it doesn't exist, congratulate without username.
+                    const char *userEnv = std::getenv("USER");
+                    if (nullptr != userEnv) {
+                        std::string userString = userEnv;
+
+                        std::cout << ", " << userString;
+                    }
+                    std::cout << "!\n";
+                }
+            }
+            catch (std::invalid_argument const& ex) {
+                std::cerr << "conversion error: " << ex.what() << std::endl;
+            }
+            catch (std::out_of_range const& ex) {
+                std::cerr << "conversion error: " << ex.what() << std::endl;
+            }
         }
-
-
-        // TODO: If the birthdate matches the current date, congratulate the user
     }
     else {
         std::cout << "No BIRTHDATE environment variable found" << std::endl;  // TODO: remove this
     }
+
+    std::cout << "end program" << std::endl;
 
     return 0;
 }
