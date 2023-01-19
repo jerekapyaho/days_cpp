b546f03fa752c750065981d7af58edfc40e5a31c
Jere Kapyaho <jere.kapyaho@tuni.fi>
2023-01-19 14:37:38 +0200

    Using namespace std in limited scopes

--- days.cpp
+++ days.cpp
@@ -12,42 +12,44 @@
 // Parses the string `buf` for a date in YYYY-MM-DD format. If `buf` can be parsed,
 // returns a wrapped `std::chrono::year_month_day` instances, otherwise `std::nullopt`.
 std::optional<std::chrono::year_month_day> getDateFromString(const std::string& buf) {
-    constexpr std::string_view yyyymmdd = "YYYY-MM-DD";
+    using namespace std;  // use std facilities without prefix inside this function
+
+    constexpr string_view yyyymmdd = "YYYY-MM-DD";
     if (buf.size() != yyyymmdd.size()) {
-        return std::nullopt;
+        return nullopt;
     }
 
-    std::istringstream input(buf);
-    std::string part;
-    std::vector<std::string> parts;
-    while (std::getline(input, part, '-')) {
+    istringstream input(buf);
+    string part;
+    vector<string> parts;
+    while (getline(input, part, '-')) {
         parts.push_back(part);
     }
     if (parts.size() != 3) {  // expecting three components, year-month-day
-        return std::nullopt;
+        return nullopt;
     }
 
     int year{0};
     unsigned int month{0};
     unsigned int day{0};
     try {
-        year = std::stoul(parts.at(0));
-        month = std::stoi(parts.at(1));
-        day = std::stoi(parts.at(2));
+        year = stoul(parts.at(0));
+        month = stoi(parts.at(1));
+        day = stoi(parts.at(2));
 
-        return std::chrono::year_month_day{
-            std::chrono::year{year},
-            std::chrono::month(month),
-            std::chrono::day(day)};
+        return chrono::year_month_day{
+            chrono::year{year},
+            chrono::month(month),
+            chrono::day(day)};
     }
-    catch (std::invalid_argument const& ex) {
-        std::cerr << "conversion error: " << ex.what() << std::endl;
+    catch (invalid_argument const& ex) {
+        cerr << "conversion error: " << ex.what() << endl;
     }
-    catch (std::out_of_range const& ex) {
-        std::cerr << "conversion error: " << ex.what() << std::endl;
+    catch (out_of_range const& ex) {
+        cerr << "conversion error: " << ex.what() << endl;
     }
 
-    return std::nullopt;
+    return nullopt;
 }
 
 // Returns the value of the environment variable `name` as an `std::optional``
@@ -63,10 +65,12 @@ std::optional<std::string> getEnvironmentVariable(const std::string& name) {
 }
 
 int main() {
+    using namespace std;
+
     // Get the current date from the system clock and extract year_month_day.
     // See https://en.cppreference.com/w/cpp/chrono/year_month_day
-    const std::chrono::time_point now = std::chrono::system_clock::now();
-    const std::chrono::year_month_day currentDate{std::chrono::floor<std::chrono::days>(now)};
+    const chrono::time_point now = chrono::system_clock::now();
+    const chrono::year_month_day currentDate{chrono::floor<chrono::days>(now)};
 
     // Check the birthdate and user with generic helper functions
     auto birthdateValue = getEnvironmentVariable("BIRTHDATE");
@@ -75,7 +79,7 @@ int main() {
         if (birthdate.has_value()) {
             auto b = birthdate.value();
             if (b.month() == currentDate.month() && b.day() == currentDate.day()) {
-                std::ostringstream message;
+                ostringstream message;
                 message << "Happy birthday";
                 auto userEnv = getEnvironmentVariable("USER");
                 if (userEnv.has_value()) {
@@ -83,11 +87,13 @@ int main() {
                     message << ", " << user;
                 }
                 message << "!\n";
-                std::cout << message.str();
+                cout << message.str();
             }
         }
     }
 
+    // Add a few events for testing, then print them out:
+    
     Event ev1(
         "1989-11-09", 
         "history", 
@@ -98,19 +104,9 @@ int main() {
         "Finland joined the European Union");
     Event ev3("2010-10-01", "economy", "Something big");
 
-    std::cout << ev1.getTimestamp() << ": " << ev1.getDescription() << '\n';
-    std::cout << ev2.getTimestamp() << ": " << ev2.getDescription() << '\n';
-    std::cout << ev3.getTimestamp() << ": " << ev3.getDescription() << '\n';
+    cout << ev1.getTimestamp() << ": " << ev1.getDescription() << '\n';
+    cout << ev2.getTimestamp() << ": " << ev2.getDescription() << '\n';
+    cout << ev3.getTimestamp() << ": " << ev3.getDescription() << '\n';
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
